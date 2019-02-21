f2e70c074218d50439603792e8a7c2f05f942788
dkam26 <gkam1989@gmail.com>
2019-02-21 17:37:01 +0300

    Add a user model

---
+++ my_app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
+import psycopg2, os
+from flask_cors import CORS
+db_url='postgresql://kamaradeo:1234@localhost/booking'
+
+app=Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY']='Random key'
+if os.environ.get('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+db=SQLAlchemy(app)
+api = Api(app)
+from my_app import views
---
+++ my_app/models/User.py
@@ -0,0 +1,25 @@
+from decimal import Decimal
+import datetime
+from my_app import db
+
+class User(db.Model):
+    """Table for users"""
+    __tablename__ = "User"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True)
+    surname = db.Column(db.String(255))
+    firstname = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    profile_picture = db.Column(db.String(255))
+    user_type = db.Column(db.String(255))
+    def __init__(self, user, surname, firstname , email, password, profile_picture):
+        self.username = user
+        self.surname = surname
+        self.firstname = firstname
+        self.email = email
+        self.password = password
+        self.profile_picture = profile_picture
+        self.user_type = user_type
+    def __repr__(self):
+        return '<User %d>'% self.id
